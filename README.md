bbf091596cb4514eaeb1d33336a4260e3d858333
David Cruse <70957440+dcruselex@users.noreply.github.com>
2021-03-01 15:37:17 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# boatmonitoring
+This project is to monitor a few of my houseboat evironment and critical systems. I will later expand to monitor additional systems.
+The objective is to monitor remotely even during the winter season when less time is spent on the boat.
+
+Initially, I will setup monitoring of temperature and water at limited locations on the boat, then expand to monitor additional locations as well as system and evnironmental conditions.
+
+I will use a Raspberry Pi and other electronics to create the solution in addition to the Python code.
+In additon, I will use an older tablet and small keyboard as the interface which I will locate in the houseboat controls area.    Remotely, it will be via a secure website.
+
+This project will include using Raspberry Pi 3, breadboard, etc..
+
+This will include initially moisture and temperature
+
+
---
+++ boatmon.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import re
+import random
+import glob
+from termcolor import colored, cprint
+
+def printHeader():
+    print()
+    cprint("██████╗░░█████╗░░█████╗░████████╗░░░░░░███╗░░░███╗░█████╗░███╗░░██╗██╗████████╗░█████╗░██████╗░", "green")
+    cprint("██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝░░░░░░████╗░████║██╔══██╗████╗░██║██║╚══██╔══╝██╔══██╗██╔══██╗", "green")
+    cprint("██████╦╝██║░░██║███████║░░░██║░░░█████╗██╔████╔██║██║░░██║██╔██╗██║██║░░░██║░░░██║░░██║██████╔╝", "green")
+    cprint("██╔══██╗██║░░██║██╔══██║░░░██║░░░╚════╝██║╚██╔╝██║██║░░██║██║╚████║██║░░░██║░░░██║░░██║██╔══██╗", "green")
+    cprint("██████╦╝╚█████╔╝██║░░██║░░░██║░░░░░░░░░██║░╚═╝░██║╚█████╔╝██║░╚███║██║░░░██║░░░╚█████╔╝██║░░██║", "green")
+    cprint("╚═════╝░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░░░░░░░╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝", "green")
+
+
+def prompt():
+    global gest, img_counter
+    green = "green"
+    print("({})uit, ({})ext ({}) .".format(colored("Q", green), colored("N", green), colored("SPACE", green)))
+
+        
+#-----------------------main--------------------------
+os.system('color') # needed for termcolor to work on Windows
+printHeader()
+quitnow = False
+prompt()
+while True:
+    prompt()
+    k = input()
+    if k == 27 or k == ord("q") or k == ord("Q"):  # ESC or Q pressed
+        cprint("Escape/Quit. Closing...", "red")
+        break
+    
+    elif k == 32:  # SPACE bar pressed, capture two images regular/mirror
+        cprint("Spacebar...", "red")
+
+        
+    elif k==13 or k== ord("N") or k == ord("n"): # carriage return. Next class
+        cprint("Next", "red")
